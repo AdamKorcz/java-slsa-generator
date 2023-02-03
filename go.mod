80af803f454ebb96402bdc364e394fc218687473
AdamKorcz <adam@adalogics.com>
2023-02-03 16:25:51 +0000

    Refactory main.go
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- go.mod
+++ go.mod
@@ -5,30 +5,21 @@ go 1.19
 require (
 	github.com/google/go-cmp v0.5.9
 	github.com/in-toto/in-toto-golang v0.5.0
-	github.com/rhysd/actionlint v1.6.22
 	github.com/slsa-framework/slsa-github-generator v1.4.0
 )
 
 require (
 	github.com/coreos/go-oidc/v3 v3.4.0 // indirect
-	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-github/v44 v44.1.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/robfig/cron v1.2.0 // indirect
 	github.com/secure-systems-lab/go-securesystemslib v0.4.0 // indirect
 	github.com/shibumi/go-pathspec v1.3.0 // indirect
 	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/oauth2 v0.2.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,26 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
-	"os"
-	"strings"
 
 	//slsa02 "github.com/in-toto/in-toto-golang/in_toto/slsa_provenance/v0.2"
-	"github.com/rhysd/actionlint"
 	"github.com/slsa-framework/slsa-github-generator/slsa"
 
 	"github.com/AdamKorcz/java-slsa-generator/gh"
-)
-
-// flags
-var (
-	flagYmlFile = flag.String("ymlfile", "", "path to ymlfile to attest")
-)
-
-var (
-	demoBuildDir = "/home/runner/work/AdamKorcz/actions-test"
-	demoBuilder  = "https://github.com/slsa-framework/slsa-github-generator/.github/workflows/builder_java_slsa3.yml@v1.4.0"
 )
 
 type SlsaStep struct {
@@ -43,71 +29,15 @@ func NewBuildConfig(steps []SlsaStep) BuildConfig {
 	}
 }
 
-// Input: actionlint.Workflow.Jobs
-// Output: The deploy command
-func getSteps(jobs map[string]*actionlint.Job) []SlsaStep {
-	provenanceSteps := make([]SlsaStep, 0)
-	for _, job := range jobs {
-		if len(job.Steps) != 0 {
-			for _, step := range job.Steps {
-				if step.Name != nil {
-					//fmt.Println("Step name: ", step.Name.Value)
-					//fmt.Println("Step exec: ", step.Exec.Kind())
-					switch step.Exec.Kind() {
-					case 0:
-					case 1:
-						if runCmd := step.Exec.(*actionlint.ExecRun).Run; runCmd != nil {
-							runCmdSlice := strings.Split(runCmd.Value, " ")
-							if len(runCmdSlice) == 0 {
-								continue
-							}
-							if len(runCmdSlice) != 1 && runCmdSlice[len(runCmdSlice)-1] == "deploy" {
-								break
-							}
-							if len(runCmdSlice) != 1 && runCmdSlice[len(runCmdSlice)-1] == "test" {
-								break
-							}
-							provenanceStep := SlsaStep{
-								Command:    strings.Split(runCmd.Value, " "),
-								WorkingDir: demoBuildDir,
-								Env:        []string{"env"},
-							}
-							provenanceSteps = append(provenanceSteps, provenanceStep)
-						}
-					}
-				}
-			}
-		}
-	}
-	return provenanceSteps
-}
-
 func main() {
-	flag.Parse()
-
-	if *flagYmlFile == "" {
-		fmt.Println("\nPlease specify a .yml file")
-		fmt.Println("The usage is: go run main.go -ymlfile=/path/to/.yml\n")
-		os.Exit(0)
-	}
-
-	b, err := os.ReadFile(*flagYmlFile)
-	if err != nil {
-		panic(err)
-	}
-	w, ymlErr := actionlint.Parse(b)
-	if err != nil {
-		panic(ymlErr)
-	}
-
 	testBuild := gh.NewTestBuild()
 	g := slsa.NewHostedActionsGenerator(testBuild).WithClients(&slsa.NilClientProvider{})
 	provenance, err := g.Generate(context.Background())
 	if err != nil {
 		panic(err)
 	}
-	steps := getSteps(w.Jobs)
-	provenance.Predicate.BuildConfig = NewBuildConfig(steps)
+
+	//provenance.Predicate.BuildConfig = NewBuildConfig(steps)
 
 	provenanceJson, err := json.MarshalIndent(provenance, "", "\t")
 	if err != nil {
